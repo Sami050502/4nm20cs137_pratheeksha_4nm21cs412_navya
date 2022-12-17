f5b1e18e4e0a4a2bafe8a40590bd5be1527d2e1a
Sami050502 <92709117+Sami050502@users.noreply.github.com>
2022-12-17 20:08:13 +0530

    Add files via upload

---
+++ foodordersystem/admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div class="wrapper">
+    
+        <p class="text-center">food ordering website <a href="#"> </a></p>
+
+    </div>
+    </div>
---
+++ foodordersystem/contact.php
@@ -0,0 +1,9 @@
+<?php include('partials-front/menu.php'); ?>
+<html>
+    <head>
+   <div class=text-center> 
+<h2>phone-no:</h2><P><b>9481537661</b><P><br><br>
+<h2>email:</h2><p><b>food@gmail.com</b></p>
+</div>
+</head>
+</html>
